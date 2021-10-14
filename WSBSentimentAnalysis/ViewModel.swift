a2b6b41b9eb68e34380edf3ceb21de6cce7541de
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-14 09:47:25 -0400

    added viewmodel

---
+++ WSBSentimentAnalysis/ViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  ViewModel.swift
+//  WSBSentimentAnalysis
+//
+//  Created by Vincent Cloutier on 2021-10-14.
+//
+
+import Foundation
+
+// ADDING THE COINS TO THE ARRAY
+// Interact with service class and handle any additional business logic
+
+protocol ViewModel: ObservableObject {
+    func getStocks() async
+}
+
+@MainActor // You need this w/ async await to signify you want to update this on the main thread
+final class ViewModelImpl: ViewModel {
+    
+    
+    @Published private(set) var coins: [Stock] = [] // access from outside can't change from outside
+    
+    private let service: Service
+    
+    init(service: Service) {
+        self.service = service
+    }
+    func getStocks() async {
+        do {
+            self.coins = try await service.fetchStocks()
+        } catch {
+            print(error)
+        }
+    }
+}
